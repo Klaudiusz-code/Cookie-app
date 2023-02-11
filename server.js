c2b67eabd81c000fc0629880f5ae6aafb747b67a
Klaudiusz <muminowy123@gmail.com>
2023-02-11 17:33:05 +0100

    Added JSON support, static public files, cookies and routes

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
+
 app.use('/',homeRouter)
 app.use('/configurator', configuratorRouter)
 
