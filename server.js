42244b15524da6dee0557a1655d48b02401e29fe
Klaudiusz <muminowy123@gmail.com>
2023-02-11 17:31:24 +0100

    Update

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const cookieParser = require('cookie-parser')
 const {engine} = require('express-handlebars')
+const {homeRouter} = require("./routes/home");
+const {configuratorRouter} = require("./routes/configurator");
 
 const app = express();
 
+app.use(express.json())
+app.use(express.static('public'))
+app.use(cookieParser())
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
-app.get('/',(req, res)=>{
-    res.render('home/index')
-})
+app.use('/',homeRouter)
+app.use('/configurator', configuratorRouter)
 
 app.listen(3000)
