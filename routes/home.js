884126165407f795fbb8558defdf64dcff2651e8
Klaudiusz <muminowy123@gmail.com>
2023-02-13 20:03:38 +0100

    Added bases and addons to home.js router

--- routes/home.js
+++ routes/home.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const {COOKIE_BASES, COOKIE_ADDONS} = require("../data/cookie-data");
+const {handlebarsExpress} = require("../handlebars-express");
 
 const homeRouter = express.Router();
 
 homeRouter
     .get('/',(req, res) => {
+
+        // const sum = handlebarsExpress['find-price'](Object.entries(COOKIE_BASES), Object.entries(COOKIE_ADDONS));
+
         res.render('home/index',{
             cookie: {
                 base: 'light',
                 addons: ['sprinkles','honey','chocolate'],
-            }
+            },
+            bases: Object.entries(COOKIE_BASES),
+            addons: Object.entries(COOKIE_ADDONS),
+
+            // sum,
         })
     })
 
