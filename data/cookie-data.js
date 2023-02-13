1d2cb38aaa797e96a21bf103b7ced4d815546713
Klaudiusz <muminowy123@gmail.com>
2023-02-13 19:54:44 +0100

    W Added database for cookie prices and information

---
+++ data/cookie-data.js
@@ -0,0 +1,16 @@
+const COOKIE_BASES = {
+    light: 5,
+    dark: 5,
+};
+
+const COOKIE_ADDONS = {
+    chocolate: 5,
+    sprinkles: 3,
+    honey: 6,
+    coconut: 10,
+};
+
+module.exports = {
+    COOKIE_BASES,
+    COOKIE_ADDONS,
+}
