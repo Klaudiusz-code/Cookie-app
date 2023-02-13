cf9d7f7424cd98f222776e15eef022190503fd97
Klaudiusz <muminowy123@gmail.com>
2023-02-13 20:04:20 +0100

    Added helpers

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const cookieParser = require('cookie-parser')
 const {engine} = require('express-handlebars')
 const {homeRouter} = require("./routes/home");
 const {configuratorRouter} = require("./routes/configurator");
+const {handlebarsExpress} = require("./handlebars-express");
 
 const app = express();
 
 app.use(express.json())
 app.use(express.static('public'))
 app.use(cookieParser())
-app.engine('.hbs', engine({extname: '.hbs'}));
+app.engine('.hbs', engine({
+    extname: '.hbs',
+    helpers: handlebarsExpress,
+}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
