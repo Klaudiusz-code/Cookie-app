6305e171d9a07b8af2e31baf720d50df7b09339f
Klaudiusz <muminowy123@gmail.com>
2023-02-14 18:07:50 +0100

    Modified homeRouter to use cookie-data and handlebars-helpers

--- routes/home.js
+++ routes/home.js
@@ -1,23 +1,30 @@
 const express = require('express');
 const {COOKIE_BASES, COOKIE_ADDONS} = require("../data/cookie-data");
-const {handlebarsExpress} = require("../handlebars-express");
+const {handlebarsExpress} = require("../utils/handlebars-helpers");
+const {getAddonsFromReq} = require("../utils/get-addons-from-req");
 
 const homeRouter = express.Router();
 
 homeRouter
     .get('/',(req, res) => {
 
-        // const sum = handlebarsExpress['find-price'](Object.entries(COOKIE_BASES), Object.entries(COOKIE_ADDONS));
+        const {cookieBase} = req.cookies;
+        const addons = getAddonsFromReq(req)
+
+        const sum = (cookieBase ? handlebarsExpress.findPrice(Object.entries(COOKIE_BASES),cookieBase) : 0)
+            + addons.reduce((prev,curr)=>(
+                prev + handlebarsExpress.findPrice(Object.entries(COOKIE_ADDONS),curr)
+            ),0)
 
         res.render('home/index',{
             cookie: {
-                base: 'light',
-                addons: ['sprinkles','honey','chocolate'],
+                base: cookieBase,
+                addons: addons,
             },
             bases: Object.entries(COOKIE_BASES),
             addons: Object.entries(COOKIE_ADDONS),
 
-            // sum,
+            sum,
         })
     })
 
