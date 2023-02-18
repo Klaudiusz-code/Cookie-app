df2cf607d2185968d7bf2927626ce15b579d298b
Klaudiusz <muminowy123@gmail.com>
2023-02-18 08:24:39 +0100

    Refactor order routes to use getCookieSettings function for cookie calculations

--- routes/order.js
+++ routes/order.js
@@ -2,39 +2,30 @@ const express = require('express');
 const {getAddonsFromReq} = require("../utils/get-addons-from-req");
 const {handlebarsExpress} = require("../utils/handlebars-helpers");
 const {COOKIE_BASES, COOKIE_ADDONS} = require("../data/cookie-data");
+const {getCookieSettings} = require("../utils/get-cookie-settings");
 
 const orderRouter = express.Router();
 
 orderRouter
     .get('/summary',(req, res) =>{
 
-        const {cookieBase} = req.cookies;
-        const addons = getAddonsFromReq(req)
-
-        const sum = (cookieBase ? handlebarsExpress.findPrice(Object.entries(COOKIE_BASES),cookieBase) : 0)
-            + addons.reduce((prev,curr)=>(
-                prev + handlebarsExpress.findPrice(Object.entries(COOKIE_ADDONS),curr)
-            ),0)
+        const {sum,addons,cookieBase,allAddons,allBases}  = getCookieSettings(req);
 
         res.render('order/summary',{
             cookie: {
                 base: cookieBase,
                 addons: addons,
             },
-            bases: Object.entries(COOKIE_BASES),
-            addons: Object.entries(COOKIE_ADDONS),
+            bases: allBases,
+            addons: allAddons,
 
             sum,
         })
     })
     .get('/thanks',(req,res) =>{
-        const {cookieBase} = req.cookies;
-        const addons = getAddonsFromReq(req)
 
-        const sum = (cookieBase ? handlebarsExpress.findPrice(Object.entries(COOKIE_BASES),cookieBase) : 0)
-            + addons.reduce((prev,curr)=>(
-                prev + handlebarsExpress.findPrice(Object.entries(COOKIE_ADDONS),curr)
-            ),0)
+        const {sum} = getCookieSettings(req);
+
         res
             .clearCookie('cookieBase')
             .clearCookie('cookieAddons')
